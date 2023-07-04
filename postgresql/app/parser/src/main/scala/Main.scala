5c96d111c4ae6ebe1b42eda5c7c692e003dc61ca
Vahe Khachatryan <vahe.khachatryan.1999@gmail.com>
2023-07-04 18:37:31 +0400

    Added parser logic

--- postgresql/app/parser/src/main/scala/Main.scala
+++ postgresql/app/parser/src/main/scala/Main.scala
@@ -1,21 +1,69 @@
 package parser
 
 import org.antlr.v4.runtime._
-import org.antlr.v4.runtime.tree.ParseTreeWalker
+import scala.collection.mutable.Map
+import scala.jdk.CollectionConverters._
+import java.util.concurrent._
+import java.util.Arrays
+import java.io._
 
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val input = "SELECT column1, column2 FROM table1, table2 WHERE column1 = 'value' AND column2 > 10"
-    val lexer = new PostgreSQLLexer(CharStreams.fromString(input))
-    val tokens = new CommonTokenStream(lexer)
-    val parser = new PostgreSQLParser(tokens)
-    val tree = parser.selectStatement()
 
-    val listener = new CustomPostgreSQLListener()
-    val walker = new ParseTreeWalker()
-    walker.walk(listener, tree)
+  def parse(query: String): (Map[String, Integer]) = {
+    val executor = Executors.newSingleThreadExecutor()
+    var result: java.util.concurrent.Future[CustomPostgreSQLListener] = null
+    val timeout = 5
+
+    var processedQuery = query.trim()
+    if (processedQuery.takeRight(1) != ";") {
+      processedQuery += ";"
+    }
+    println("processedQuery === " + processedQuery)
+
+    try {
+      val futures = executor.invokeAll(
+        Arrays.asList(new CallableParse(processedQuery)),
+        timeout,
+        TimeUnit.SECONDS
+      )
+      result = futures.get(0)
+    } catch {
+      case e: InterruptedException => {}
+    }
+
+    executor.shutdown()
+    try {
+      val listener = result.get()
+      return (listener.getFeatureMap().asScala) // , listener.getHash())
+    } catch {
+      case e: CancellationException => {
+        throw new Exception(
+          "Parser timed out after " + timeout + " seconds. query: " + query.substring(0, 20) + "..."
+        )
+      }
+      case t: Throwable => {
+        throw t
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val inputs = Array(
+      // "SELECT column1, column2 FROM table1, table2 WHERE column1 = 'value' AND column2 > 10",
+      "SELECT column3 FROM table3 WHERE column3 = 'value' AND column4 = 5",
+      "INSERT INTO table3 (col1, col2 ) VALUES ('asd', '54')"
+    )
+
+    try {
+      for(input <- inputs) {
+        val (rules) = parse(input)
+        println("rules = " + rules)
+      }
+    } catch {
+      case e: Throwable => {
+        println("error = " + e.getMessage())
+      }
+    }
   }
 }
-
-
